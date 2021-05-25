2699c4cc89e7c62dfca05d75a87f7787a2cea57b
ylj29 <2801684704@qq.com>
2021-05-25 21:22:49 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+function userNameValidator(control: FormControl): { [s: string]: boolean } {
+  if ( !control.value.match(/^zime/) ) {
+    return { invalidUser : true };
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login-app';
+  //对应登录的表单
+  myForm!: FormGroup;
+  
+  //输入用户名的输入控件
+  userName : FormControl;
+
+  //输入密码的输入控件
+  password : FormControl;
+
+  name$!: Observable<string>;
+
+  constructor(private fb: FormBuilder){
+    this.myForm = this.fb.group(
+      {
+        'userName' : ['aaa', Validators.compose([Validators.required,userNameValidator,Validators.minLength(7)])],
+        'password' : ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      }
+    );
+
+    this.userName = <FormControl>this.myForm.controls['userName'];
+    this.password = <FormControl>this.myForm.controls['password'];
+    this.name$ = this.userName.valueChanges;
+    this.userName.valueChanges.subscribe(val => {
+      console.log(val);
+      });
+      
+  }
+  onSubmit(vlaue:any){
+    console.log(vlaue)
+  }
 }
